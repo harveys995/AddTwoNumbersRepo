ce0681eb72cbabe1cd8804f1abc3c63df7425e54
EC2 Default User <ec2-user@ip-172-31-45-58.eu-west-2.compute.internal>
2023-05-03 12:21:29 +0000

    fourth  commit for AddTwoNumbers

--- add.java
+++ add.java
@@ -1,19 +1,17 @@
 public class add {
     public static void main (String[] args) {
-        
         int num1 = 10;
         int num2 = 300;
         int num3 = 5;
         int num4 = 5000000 * num3;
-        
         for (int i = 0; i < num2; i++) {
             num1++;
         }
-        
         System.out.println(num1);
         
+        
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your age:");
+        System.out.println("Enter your age:")
         int age = sc.nextInt();
         if (age < 0) {
             System.out.println("Invalid age");
