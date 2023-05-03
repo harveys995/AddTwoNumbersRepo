b576301b44bb324106510360fe189fe47676beca
EC2 Default User <ec2-user@ip-172-31-45-58.eu-west-2.compute.internal>
2023-05-03 10:37:15 +0000

    initial commit for AddTwoNumbers

---
+++ add.java
@@ -0,0 +1,13 @@
+public class add {
+    public static void main (String[] args) {
+        
+        int num1 = 10;
+        int num2 = 300;
+        
+        for (int i = 0; i < num2; i++) {
+            num1++;
+        }
+        
+        System.out.println(num1);
+    }
+}
